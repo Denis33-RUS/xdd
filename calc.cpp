0430f374868a62cc8e99b67a752a947b9be44c9b
Denis33-RUS <denis-ivanov33rus@yandex.ru>
2025-04-07 12:35:42 +0000

    сохранено

---
+++ calc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "pineapple.cpp"
+using namespace std;
+double calculated(double N1, double N2, char math) {
+    switch (math) {
+        case '+': return sum
+    }
+}
